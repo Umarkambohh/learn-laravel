8495c60fda9a9b98ab71f42658f451fe8ba56d9d
Umar Saleem <umarsaleem512452@gmail.com>
2024-05-09 15:16:48 +0500

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,8 @@
+import './bootstrap';
+
+
+import { createApp } from 'vue'
+
+const app = createApp()
+
+app.mount('#app')
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	
+		
+		@vite(['resources/js/app.js'])
+	</head>
+	<body>
+		<div id="app">
+			<!-- Vue.js components will be processed here. -->ssss
+		</div>
+	</body>
+</html>
